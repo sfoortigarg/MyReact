c32cc5f257f2a7c797cf025857a0c29cc08b9b71
Sfoorti Garg <sfoortigarg@gmail.com>
2020-09-14 10:49:53 -0700

    React animation components

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 const minLength = (len) => (val) => (val) && (val.length >= len);
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -13,14 +14,19 @@ const maxLength = (len) => (val) => !(val) || (val.length <= len);
 function RenderDish({ dish }) {
     return (
         <div className="col-12 col-md-5 m-1">
-            <Card>
-                <CardImg src={baseUrl + dish.image}
-                    alt={dish.name} />
-                <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>
-                    <CardText>{dish.description}</CardText>
-                </CardBody>
-            </Card>
+            <FadeTransform in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
+                <Card>
+                    <CardImg src={baseUrl + dish.image}
+                        alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            </FadeTransform>
         </div>
     );
 }
@@ -103,8 +109,10 @@ function RenderComments({ comments, postComment, dishId}) {
             <div className="col-12 col-md-5 m-1">
                 <h4>Comments</h4>
                 <ul className="list-unstyled">
+                    <Stagger in>
                     {comments.map((comment) => {
                         return (
+                            <Fade in>
                             <li key={comment.id}>
                                 <p>{comment.comment}</p>
                                 <p>--{comment.author} ,
@@ -114,8 +122,10 @@ function RenderComments({ comments, postComment, dishId}) {
                                     day: '2-digit'
                                 }).format(new Date(Date.parse(comment.date)))}</p>
                             </li>
+                            </Fade>
                         );
                     })}
+                    </Stagger>
                 </ul>
                 <CommentForm dishId={dishId} postComment={postComment}/>
             </div>
